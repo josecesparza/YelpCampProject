2883a13310df7da4132dc15302b094474b88bbbe
jose <josecarlosesparza@gmail.com>
2019-07-06 01:04:43 +0100

    Landing page and directory views created

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
 
 app.listen(3000, process.env.IP, function(){
     console.log("The YelpCamp Server has started!");
