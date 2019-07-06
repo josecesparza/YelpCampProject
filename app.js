99a7f4a64534f5ba5128490b73b9624abb674ea9
jose <josecarlosesparza@gmail.com>
2019-07-06 01:39:30 +0100

    body-parser added and campgrounds post created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
@@ -15,6 +17,11 @@ app.get("/campgrounds", function (req, res) {
     ]
 
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    //redirect back to campgrounds page
 });
 
 app.listen(3000, process.env.IP, function () {
