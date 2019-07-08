7c19509a597e367f0fa8105fc42f7eeaf9a89f3b
jose <josecarlosesparza@gmail.com>
2019-07-09 00:34:53 +0100

    Changes in campgrounds.ejs header

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    { name: "Salmon Creek", image: "https://pixabay.com/get/5fe8d1434852b108f5d084609620367d1c3ed9e04e50744f772c78d0944cc5_340.jpg" },
-    { name: "Granite Hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732d78d7934ec458_340.jpg" },
-    { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c732d78d7934ec458_340.jpg" }
+    { name: "Salmon Creek", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732d7dd5974ac75d_340.jpg" },
+    { name: "Granite Hill", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732d7dd5974ac75d_340.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/52e3d5404957a514f6da8c7dda793f7f1636dfe2564c704c732d7dd5974ac75d_340.jpg" }
 ]
 
 app.get("/", function (req, res) {
