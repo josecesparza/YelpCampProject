3582e5aff9ce92c0fd9f2e284fbbb9d9a55dbce5
jose <josecarlosesparza@gmail.com>
2019-07-30 12:48:17 +0100

    Register form and routes created

--- app.js
+++ app.js
@@ -122,6 +122,28 @@ app.post("/campgrounds/:id/comments", function (req, res) {
     });
 });
 
+//========================
+//AUTH ROUTES
+//========================
+//Show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+//Handling sign up logic 
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
 
 app.listen(3000, process.env.IP, function () {
     console.log("The YelpCamp Server has started!");
