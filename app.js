409a75fc55dbac86d85c7036520935969c7dade1
jose <josecarlosesparza@gmail.com>
2019-07-30 21:40:08 +0100

    Checking if the user is logged in to add a new comment

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/campgrounds/:id", function (req, res) {
 //========================
 //COMMENTS ROUTES
 //========================
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
             console.log(err);
@@ -97,7 +97,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     //Lookup campground using ID
     Campground.findById(req.params.id, function (err, campground) {
         if (err) {
@@ -167,6 +167,13 @@ app.get("/logout", function (req, res) {
     req.logOut();
     res.redirect("/campgrounds");
 });
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(3000, process.env.IP, function () {
     console.log("The YelpCamp Server has started!");
