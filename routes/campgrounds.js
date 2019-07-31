bbb4a7f5b7f355624b78bf5dac027c6b72fb0c66
jose <josecarlosesparza@gmail.com>
2019-07-31 18:49:06 +0200

    Check auth of campgrounds to delete/edit it

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,14 +57,14 @@ router.get("/:id", function (req, res) {
 });
 
 //EDIT CAMPGROUND 
-router.get("/:id/edit", function (req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCampground) {
         if (err) {
             res.redirect("/campgrounds");
         } else {
             res.render("campgrounds/edit", { campground: foundCampground });
         }
-    })
+    });
 });
 
 //UPDATE CAMPGROUND
@@ -97,6 +97,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+};
+
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function (err, foundCampground) {
+            if (err) {
+                res.redirect("/campgrounds");
+            } else {
+                //Does user own the campground?
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 };
 
 module.exports = router;
