a05b64a43cfd98cafc6aa85d3a7ce2777b9de473
jose <josecarlosesparza@gmail.com>
2019-07-31 17:48:28 +0200

    Delete campground function added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -80,6 +80,17 @@ router.put("/:id", function (req, res) {
     });
 });
 
+//DESTROY CAMPGROUND
+router.delete("/:id", function (req, res) {
+    Campground.findByIdAndRemove(req.params.id, function (err) {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
 //Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
