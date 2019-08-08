36d996b59d10ef8ab9d19ccf4e6f8231d82c7065
jose <josecarlosesparza@gmail.com>
2019-08-09 01:25:11 +0200

    Add creation comment date

--- models/comment.js
+++ models/comment.js
@@ -7,7 +7,11 @@ var commentSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: "User"
         },
-        username: String
+        username: String,
+    },
+    created: {
+        type: Date,
+        default: Date.now()
     }
 });
 
